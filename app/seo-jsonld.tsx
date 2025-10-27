ac7f51072b1ab67c68c4210382b1ac8a46e925c7
Avocat <valentin.gil@epitech.eu>
2025-10-27 10:52:35 +0100

    Update job title and description in structured data and web manifest for clarity

--- app/seo-jsonld.tsx
+++ app/seo-jsonld.tsx
@@ -8,8 +8,8 @@ export const siteStructuredData = {
     "https://github.com/valentin-gil",
     "https://www.linkedin.com/in/valentin-gil-12741a38a/"
   ],
-  "jobTitle": "Étudiant en BUT Métiers du Multimédia et de l'Internet",
-  "description": "Portfolio de Valentin Gil, étudiant en BUT MMI à l'IUT d'Angoulême",
+  "jobTitle": "Étudiant de 2e année en BUT Métiers du Multimédia et de l'Internet à l'IUT d'Angoulême.",
+  "description": "Portfolio de Valentin Gil, étudiant de 2e année en BUT MMI à l'IUT d'Angoulême.",
   "worksFor": {
     "@type": "Organization",
     "name": "Valentin Gil"
